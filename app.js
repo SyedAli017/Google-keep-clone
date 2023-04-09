fa9884bc5eab1274a5c4e7a4b64a8b08147e1c07
SyedAli017 <sydali3103@gmail.com>
2023-04-10 05:28:42 +0530

    display notes on the app

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 class App {
   constructor() {
+    this.notes = [];
+
     this.$form = document.querySelector(".form");
-    this.$noteTitle = document.querySelector(".note-title");
-    this.$noteText = document.querySelector(".note-text");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
     this.$buttons = document.querySelector(".buttons");
+    this.$notes = document.querySelector(".notes");
+    this.$placeholder = document.querySelector(".placeholder");
 
     this.eventListeners();
   }
@@ -12,6 +16,19 @@ class App {
     document.body.addEventListener("click", (event) => {
       this.formClick(event);
     });
+
+    this.$form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const noteTitle = this.$noteTitle.value;
+      const noteText = this.$noteText.value;
+
+      const isNotePresent = noteTitle || noteText;
+
+      if (isNotePresent) {
+        this.addNote({ noteTitle, noteText });
+      }
+    });
   }
 
   formClick(event) {
@@ -19,6 +36,7 @@ class App {
 
     if (formClicked) {
       this.formOpen();
+      console.log("clicked");
     } else {
       this.formClose();
     }
@@ -32,6 +50,45 @@ class App {
   formClose() {
     this.$noteTitle.style.display = "none";
     this.$buttons.style.display = "none";
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.noteTitle,
+      text: note.noteText,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+    };
+
+    this.notes = [...this.notes, newNote];
+
+    this.showNotes();
+  }
+
+  showNotes() {
+    const isNotePresent = this.notes.length > 0;
+    if (isNotePresent) {
+      this.$placeholder.style.display = "none";
+    } else {
+      this.$placeholder.style.display = "flex";
+    }
+
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
+    <div style="background: ${note.color};" class="note">
+      <div class="${note.title && "note-title"}">${note.title}</div>
+      <div class="note-text">${note.text}</div>
+      <div class="toolbar-section">
+        <div class="toolbar">
+            <i class="fa-solid fa-palette"></i>
+            <i class="fa-solid fa-trash"></i>
+        </div>
+      </div>
+    </div>
+ `
+      )
+      .join("");
   }
 }
 
